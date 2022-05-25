9539ce9e06b65b1085d049903cb0f6bef07f975b
Robert-Drexler <drexler4@gmail.com>
2022-05-25 17:16:02 -0400

    Nav Bar Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from './Components/SideMenu'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
-const  App = () => {
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <nav>
+      
+    </nav>
+    <div>
+    <Sidebar />
     </div>
+      {/* <fieldset>
+        <legend>App.js</legend>
+        
+        <Routes>
+          
+
+        </Routes>
+      </fieldset> */}
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/Components/SideMenu.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect, useRef } from 'react';
+import Logo from "../Components/Logo.png";
+import {BiHomeAlt} from "react-icons/bi" 
+import { IconContext } from "react-icons";
+
+
+export default function SideMenu() {
+  const [isSideMenu, setSideMenu] = useState(false)
+  const open = (isSideMenu) => {
+    return setSideMenu(!isSideMenu)
+  }
+  const domeNode = useRef()
+  const updateState = (event) => {
+    if (domeNode.current.contains(event.target)) {
+      return
+    }
+    setSideMenu(false)
+  }
+  useEffect(() => {
+    document.addEventListener('onClick', updateState)
+    return () => {
+      document.removeEventListener('onClick', updateState)
+    }
+  }, [])
+  return (
+    <>
+      <header className="topBar">
+        <div className="menuBar">
+          <div className='titleBox'>
+            <div className='title-header'>
+            <img className='logo' src={Logo} alt="" />  
+            <h2>Financial Portal</h2>
+            </div>
+          </div>
+          <span
+            ref={domeNode}
+            className="navIcon"
+            onClick={() => {
+              open(isSideMenu)
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              {isSideMenu ? (
+                <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" />
+              ) : (
+                <path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" />
+              )}
+            </svg>
+          </span>
+        </div>
+        <div className="sideMenu" style={{ left: isSideMenu ? '0' : '-175px' }}>
+          <div className='navItems'>
+            <a href="#">Dashboard</a>
+            <BiHomeAlt size={28} color="white" />
+          </div>
+          <div className='navItems'>
+            <select name="" id="">
+
+            </select>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
