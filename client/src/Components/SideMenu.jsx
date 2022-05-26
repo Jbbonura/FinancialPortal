965e040a725f156ef2e3330b7ac172e560732a30
Robert-Drexler <drexler4@gmail.com>
2022-05-26 14:44:18 -0400

    New Components
    Dashboard
    View - Maiin

--- client/src/Components/SideMenu.jsx
+++ client/src/Components/SideMenu.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom'
 import Logo from "../Components/Logo.png";
 import {BiHomeAlt} from "react-icons/bi" 
-import { IconContext } from "react-icons";
+import {MdAccountBalance} from "react-icons/md"
 
 
 export default function SideMenu() {
@@ -22,6 +23,10 @@ export default function SideMenu() {
       document.removeEventListener('onClick', updateState)
     }
   }, [])
+
+
+
+
   return (
     <>
       <header className="topBar">
@@ -30,21 +35,13 @@ export default function SideMenu() {
             <div className='title-header'>
             <img className='logo' src={Logo} alt="" />  
             <h2>Financial Portal</h2>
+            <Link to='/'>Logout</Link>
             </div>
           </div>
-          <span
-            ref={domeNode}
-            className="navIcon"
-            onClick={() => {
-              open(isSideMenu)
-            }}
-          >
+          <span ref={domeNode} className="navIcon" onClick={() => {open(isSideMenu)}} >
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
+              id='toggleIcon'
+              xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               {isSideMenu ? (
                 <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" />
               ) : (
@@ -54,15 +51,42 @@ export default function SideMenu() {
           </span>
         </div>
         <div className="sideMenu" style={{ left: isSideMenu ? '0' : '-175px' }}>
-          <div className='navItems'>
-            <a href="#">Dashboard</a>
-            <BiHomeAlt size={28} color="white" />
-          </div>
-          <div className='navItems'>
-            <select name="" id="">
+            
+            <div className="navSelect" >
+            <menu>
+              <div className='menuItem' >
+                <div className='navItems'>
+                  <a href="#">Dashboard</a>
+                  <BiHomeAlt size={28} color="white" />
+                </div>
+                  <menu>
+                    <div className='menuItem'>
+                      <a href="#">about</a>
+                    </div>
+                    <div className='menuItem'>
+                      <a href="#">about</a>
+                    </div>
+                  </menu>
+              </div>
+            </menu>
 
-            </select>
-          </div>
+            <menu>
+              <div className='menuItem' >
+                <div className='navItems'>
+                  <a href="#">Bank Accounts</a>
+                  <MdAccountBalance size={28} color="white" />
+                </div>
+                  <menu>
+                    <div className='menuItem'>
+                      <a href="#">about</a>
+                    </div>
+                    <div className='menuItem'>
+                      <a href="#">about</a>
+                    </div>
+                  </menu>
+              </div>
+            </menu>
+          </div> 
         </div>
       </header>
     </>
---
+++ client/src/views/Main.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+import SideMenu from '../components/SideMenu'
+import Dashboard from '../components/Dashboard'
+
+const Main = () => {
+
+    //STAT TO KEEP TRACK OF REFRESH
+
+
+    return (
+        <fieldset>
+            <legend>Main.jsx</legend>
+
+        </fieldset>
+    )
+}
+
+export default Main
